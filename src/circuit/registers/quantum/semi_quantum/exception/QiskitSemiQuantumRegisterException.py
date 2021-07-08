c8e724fbd5c6e30d43b2be10a16994e1cd832f74
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-08 17:48:02 +0100

    New Class for Exceptions of Semi-Quantum Registers added

---
+++ src/circuit/registers/quantum/semi_quantum/exception/QiskitSemiQuantumRegisterException.py
@@ -0,0 +1,74 @@
+"""
+
+© Copyright Qiskrypt, 2021 - All rights reserved.
+
+Powered by:
+- IBM
+- IBM Quantum
+- IBM Qiskit
+
+Description:
+- The Qiskrypt is a software suite of protocols of
+  quantum cryptography, quantum communication and
+  other protocols/algorithms, built using the IBM's Qiskit.
+
+College(s):
+- NOVA School of Science and Technology, NOVA University of Lisbon, Portugal.
+- Faculty of Sciences, University of Lisbon, Portugal.
+- Tecnico Lisboa, University of Lisbon, Portugal.
+- School of Engineering, University of Connecticut, United States of America.
+
+Other Institution(s):
+- Instituto de Telecomunicacoes, Portugal.
+- Security and Quantum Information Group, Portugal.
+- LASIGE, Portugal.
+- UT Austin Program, Portugal.
+
+Author(s):
+- Ruben Barreiro (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+Supervisor(s):
+- Prof. Andre Souto (Faculty of Sciences, University of Lisbon, Portugal).
+
+Acknowledgement(s):
+- Prof. Paulo Mateus (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Nikola Paunkovic (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Walter Krawec (School of Engineering, University of Connecticut, United States of America).
+- Prof. Antonio Ravara (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+"""
+
+MESSAGE_UNSUPPORTED_OPERATIONS_EXCEPTION = "Unsupported operation for the Semi-Quantum Registers...\n"\
+                                           "The Semi-Quantum Registers only supports the operations:\n" \
+                                           "   (i)            Z-MEASUREMENT: Measurement of the Qubit in Computational Basis.\n" \
+                                           "  (ii)                  REFLECT: Reflection of the Qubit.\n" \
+                                           " (iii) CLASSICAL-STATE-CREATION: Creation of a Qubit in a Classical State.\n"
+"""
+The custom defined message for the Unsupported Operation Error for
+the IBM's Qiskit Semi-Quantum Register.
+"""
+
+
+class QiskitSemiQuantumRegisterUnsupportedOperationError(Exception):
+    """
+    Object Class of the Unsupported Operation Error for
+    the IBM's Qiskit Semi-Quantum Register.
+    """
+
+    def __init__(self, message=MESSAGE_UNSUPPORTED_OPERATIONS_EXCEPTION):
+        """
+        Constructor for the Unsupported Operation Error for
+        the IBM's Qiskit Semi-Quantum Register.
+        :param message: The custom message for the Unsupported Operation Error for
+                        the IBM's Qiskit Semi-Quantum Register.
+        """
+        self.message = message
+        """
+        Set the custom message for the Unsupported Operation Error for
+        the IBM's Qiskit Semi-Quantum Register.
+        """
+
+        super().__init__(self.message)
+        """
+        Calls the constructor of the super-class Exception.
+        """
