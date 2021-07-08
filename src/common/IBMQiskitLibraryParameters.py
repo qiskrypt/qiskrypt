ec7baa376b0c05cf53661a8897a1051e1a544605
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-08 15:59:52 +0100

    New Class for the IBM Qiskit's Library Parameters added

---
+++ src/common/IBMQiskitLibraryParameters.py
@@ -0,0 +1,48 @@
+"""
+
+© Copyright Qiskrypt, 2021 - All rights reserved.
+
+Powered by:
+- IBM
+- IBM Quantum
+- IBM Qiskit
+
+Description:
+- The Qiskrypt is a software suite of protocols of
+  quantum cryptography, quantum communication and
+  other protocols/algorithms, built using the IBM's Qiskit.
+
+College(s):
+- NOVA School of Science and Technology, NOVA University of Lisbon, Portugal.
+- Faculty of Sciences, University of Lisbon, Portugal.
+- Tecnico Lisboa, University of Lisbon, Portugal.
+- School of Engineering, University of Connecticut, United States of America.
+
+Other Institution(s):
+- UT Austin Program, Portugal.
+- Instituto de Telecomunicacoes, Portugal.
+- Security and Quantum Information Group, Portugal.
+- LASIGE, Portugal.
+
+Author(s):
+- Ruben Andre Barreiro (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+Acknowledgement(s):
+- Prof. Andre Nuno Souto (Faculty of Sciences, University of Lisbon, Portugal).
+- Prof. Paulo Alexandre Mateus (Tecnico Lisboa, University of Lisbon, and UT Austin Program, Portugal).
+- Prof. Nikola Paunkovic (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Walter Krawec Nuno Souto (School of Engineering, University of Connecticut, United States of America).
+- Prof. Antonio Maria Ravara (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+"""
+
+# The Enumerations and Constants
+
+# The maximum number of Qubits for the QASM Simulator of
+# the IBM's Qiskit Library
+QASM_SIMULATOR_MAX_NUM_QUBITS = 32
+
+# The default number of experiment counts for the final result,
+# for simulation or executions of quantum circuits,
+# using the IBM's Qiskit Library
+DEFAULT_NUM_EXPERIMENT_COUNTS = 1000
