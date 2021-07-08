f4e11c560437d1fd1efb61e58ce328e9cc85e755
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-08 21:15:02 +0100

    New Object Class for the Qiskrypt's Quantum Registers Exceptions added

---
+++ src/circuit/registers/quantum/exception/QiskryptQuantumRegisterException.py
@@ -0,0 +1,69 @@
+"""
+
+Copyrights:\n
+- © Qiskrypt, 2021 - All rights reserved.\n
+
+Powered by:\n
+- IBM
+- IBM Quantum
+- IBM Qiskit
+
+
+Description:\n
+- The Qiskrypt is a software suite of protocols of
+  quantum cryptography, quantum communication and
+  other protocols/algorithms, built using the IBM's Qiskit.
+
+College(s):\n
+- NOVA School of Science and Technology, NOVA University of Lisbon, Portugal.
+- Faculty of Sciences, University of Lisbon, Portugal.
+- Tecnico Lisboa, University of Lisbon, Portugal.
+- School of Engineering, University of Connecticut, United States of America.
+
+Other Institution(s):\n
+- Instituto de Telecomunicacoes, Portugal.
+- SQIG, Portugal.
+- LASIGE, Portugal.
+- UT Austin Program, Portugal.
+
+Author(s):\n
+- Ruben Barreiro (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+Acknowledgement(s):\n
+- Prof. Andre Souto (Faculty of Sciences, University of Lisbon, Portugal).
+- Prof. Paulo Mateus (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Nikola Paunkovic (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Walter Krawec (School of Engineering, University of Connecticut, United States of America).
+
+"""
+
+MESSAGE_NOT_QUANTUM_REGISTER_EXCEPTION = "Invalid Register: It was expected a Qiskrypt's Quantum Register as argument!!!\n"
+"""
+The custom defined message for the Not a Quantum Register Error for
+the Qiskrypt's Quantum Register.
+"""
+
+
+class QiskryptNotClassicalRegisterError(Exception):
+    """
+    Object Class of the Not a Quantum Register Error for
+    the Qiskrypt's Quantum Register.
+    """
+
+    def __init__(self, message=MESSAGE_NOT_QUANTUM_REGISTER_EXCEPTION):
+        """
+        Constructor for the Not a Quantum Register Error for
+        the Qiskrypt's Quantum Register.
+        :param message: The custom message for the Not a Quantum Register Error for
+                        the Qiskrypt's Quantum Register.
+        """
+        self.message = message
+        """
+        Set the custom message for the Not a Quantum Register Error for
+        the Qiskrypt's Quantum Register.
+        """
+
+        super().__init__(self.message)
+        """
+        Calls the constructor of the super-class Exception.
+        """
