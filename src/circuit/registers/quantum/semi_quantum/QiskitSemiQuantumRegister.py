ebc214cfda6fb68f5e65f694d4712cf7ed6ec6c2
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-08 18:13:25 +0100

    Some minor changes added

--- src/circuit/registers/quantum/semi_quantum/QiskitSemiQuantumRegister.py
+++ src/circuit/registers/quantum/semi_quantum/QiskitSemiQuantumRegister.py
@@ -1,33 +1,35 @@
 """
 
-© Copyright Qiskrypt, 2021 - All rights reserved.
+Copyrights:\n
+- © Qiskrypt, 2021 - All rights reserved.\n
 
-Powered by:
+Powered by:\n
 - IBM
 - IBM Quantum
 - IBM Qiskit
 
-Description:
+
+Description:\n
 - The Qiskrypt is a software suite of protocols of
   quantum cryptography, quantum communication and
   other protocols/algorithms, built using the IBM's Qiskit.
 
-College(s):
+College(s):\n
 - NOVA School of Science and Technology, NOVA University of Lisbon, Portugal.
 - Faculty of Sciences, University of Lisbon, Portugal.
 - Tecnico Lisboa, University of Lisbon, Portugal.
 - School of Engineering, University of Connecticut, United States of America.
 
-Other Institution(s):
+Other Institution(s):\n
 - Instituto de Telecomunicacoes, Portugal.
 - Security and Quantum Information Group, Portugal.
 - LASIGE, Portugal.
 - UT Austin Program, Portugal.
 
-Author(s):
+Author(s):\n
 - Ruben Barreiro (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
 
-Acknowledgement(s):
+Acknowledgement(s):\n
 - Prof. Andre Souto (Faculty of Sciences, University of Lisbon, Portugal).
 - Prof. Paulo Mateus (Tecnico Lisboa, University of Lisbon, Portugal).
 - Prof. Nikola Paunkovic (Tecnico Lisboa, University of Lisbon, Portugal).
