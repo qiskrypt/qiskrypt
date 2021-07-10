95bd5590b4ee53858efaafab40d1157b0720c5c2
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-10 20:19:19 +0100

    Object class for the Qiskrypt's Ancilla Fully-Quantum Register added

---
+++ src/circuit/registers/quantum/fully_quantum/QiskryptAncillaFullyQuantumRegister.py
@@ -0,0 +1,124 @@
+"""
+
+Copyrights:\n
+- © Qiskrypt, 2021 - All rights reserved.\n
+
+Powered by:\n
+- IBM
+- IBM Quantum
+- IBM Qiskit
+
+
+Description:\n
+- The Qiskrypt is a software suite of protocols of
+  quantum cryptography, quantum communication and
+  other protocols/algorithms, built using the IBM's Qiskit.
+
+College(s):\n
+- NOVA School of Science and Technology, NOVA University of Lisbon, Portugal.
+- Faculty of Sciences, University of Lisbon, Portugal.
+- Tecnico Lisboa, University of Lisbon, Portugal.
+- School of Engineering, University of Connecticut, United States of America.
+
+Other Institution(s):\n
+- Instituto de Telecomunicacoes, Portugal.
+- SQIG, Portugal.
+- LASIGE, Portugal.
+- UT Austin Program, Portugal.
+
+Author(s):\n
+- Ruben Barreiro (NOVA School of Science and Technology, NOVA University of Lisbon, Portugal).
+
+Acknowledgement(s):\n
+- Prof. Andre Souto (Faculty of Sciences, University of Lisbon, Portugal).
+- Prof. Paulo Mateus (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Nikola Paunkovic (Tecnico Lisboa, University of Lisbon, Portugal).
+- Prof. Walter Krawec (School of Engineering, University of Connecticut, United States of America).
+
+"""
+
+"""
+Import required Libraries and Packages.
+"""
+
+from src.circuit.registers.quantum.QiskryptAncillaQuantumRegister import QiskryptAncillaQuantumRegister
+"""
+Import the Qiskrypt's Ancilla Quantum Register.
+"""
+
+from src.circuit.registers.quantum.fully_quantum.exception.QiskryptAncillaFullyQuantumRegisterException \
+    import QiskryptNotAncillaFullyQuantumRegisterError
+"""
+Import the Not Ancilla Fully-Quantum Register Error for the Qiskrypt's Ancilla Fully-Quantum Register.
+"""
+
+
+class QiskryptAncillaFullyQuantumRegister(QiskryptAncillaQuantumRegister):
+    """
+    Object Class of the Qiskrypt's Ancilla Fully-Quantum Register.
+    """
+
+    def __init__(self, name="anc_fully_qu_reg", num_ancilla_qubits=1, ancilla_quantum_register=None):
+        """
+        Constructor for the Qiskrypt's Ancilla Fully-Quantum Register.
+
+        It calls the constructor of the super-class Qiskrypt's Ancilla Quantum Register.
+
+        :param name: The name of the Qiskrypt's Ancilla Fully-Quantum Register.
+        :param num_ancilla_qubits: The number of ancilla qubits of the Qiskrypt's Ancilla Fully-Quantum Register.
+        :param ancilla_quantum_register: A built-in ancilla quantum register object of
+                                         the IBM's Qiskit Ancilla Quantum Register.
+        """
+
+        super().__init__(name=name, num_ancilla_qubits=num_ancilla_qubits, ancilla_quantum_register=ancilla_quantum_register)
+        """
+        Calls the constructor of the super-class Qiskrypt's Ancilla Quantum Register.
+        """
+
+    def get_name(self):
+        """
+        Return the name of the Qiskrypt's Ancilla Fully-Quantum Register.
+
+        :return super().get_name(): the name of the Qiskrypt's Ancilla Fully-Quantum Register.
+        """
+
+        return super().get_name()
+
+    def get_num_ancilla_qubits(self):
+        """
+        Return the number of ancilla qubits of the Qiskrypt's Ancilla Fully-Quantum Register.
+
+        :return super().get_num_ancilla_qubits(): the number of ancilla qubits of
+                                                  the Qiskrypt's Ancilla Fully-Quantum Register.
+        """
+
+        return super().get_num_ancilla_qubits()
+
+    def get_ancilla_fully_quantum_register(self):
+        """
+        Return the IBM's Qiskit Ancilla Quantum Register of the Qiskrypt's Ancilla Fully-Quantum Register.
+
+        :return super().get_ancilla_quantum_register(): the IBM's Qiskit Ancilla Quantum Register of
+                                                        the Qiskrypt's Ancilla Fully-Quantum Register.
+        """
+
+        return super().get_ancilla_quantum_register()
+
+    @staticmethod
+    def raise_not_ancilla_fully_quantum_register_error():
+        """
+        Return/Raise a Not an Ancilla Fully-Quantum Register Error for the Qiskrypt's Ancilla Fully-Quantum Register.
+
+        :raise not_ancilla_fully_quantum_register_error: a Not an Ancilla Fully-Quantum Register Error for
+                                                         the Qiskrypt's Ancilla Fully-Quantum Register.
+        """
+
+        not_ancilla_fully_quantum_register_error = QiskryptNotAncillaFullyQuantumRegisterError()
+        """
+        Retrieve the Not an Ancilla Fully-Quantum Register Error for the Qiskrypt's Ancilla Fully-Quantum Register.
+        """
+
+        """
+        Raise the Not a Fully-Quantum Register Error for the Qiskrypt's Fully-Quantum Register.
+        """
+        raise not_ancilla_fully_quantum_register_error
