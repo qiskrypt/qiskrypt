68fa153cd184913c62cb7e3d34cc3bd6ff7e18b9
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-10 18:40:36 +0100

    Some minor changes in the Qiskrypt's Ancilla Semi-Quantum Register added

--- src/circuit/registers/quantum/semi_quantum/QiskryptAncillaSemiQuantumRegister.py
+++ src/circuit/registers/quantum/semi_quantum/QiskryptAncillaSemiQuantumRegister.py
@@ -46,101 +46,102 @@ from src.circuit.registers.quantum.QiskryptAncillaQuantumRegister import Qiskryp
 Import the Qiskrypt's Ancilla Quantum Register.
 """
 
-from src.circuit.registers.quantum.semi_quantum.exception.QiskryptSemiQuantumRegisterException \
-    import QiskryptSemiQuantumRegisterUnsupportedOperationError
+from src.circuit.registers.quantum.semi_quantum.exception.QiskryptAncillaSemiQuantumRegisterException \
+    import QiskryptAncillaSemiQuantumRegisterUnsupportedOperationError
 """
-Import the Unsupported Operation Error for the Qiskrypt's Semi-Quantum Register.
+Import the Unsupported Operation Error for the Qiskrypt's Ancilla Semi-Quantum Register.
 """
 
 from src.circuit.registers.quantum.semi_quantum.exception.QiskryptAncillaSemiQuantumRegisterException \
-    import QiskryptNotSemiQuantumRegisterError
+    import QiskryptNotAncillaSemiQuantumRegisterError
 """
-Import the Not Semi-Quantum Register Error for the Qiskrypt's Semi-Quantum Register.
+Import the Not Ancilla Semi-Quantum Register Error for the Qiskrypt's Ancilla Semi-Quantum Register.
 """
 
 
 class QiskryptAncillaSemiQuantumRegister(QiskryptAncillaQuantumRegister):
     """
-    Object Class of the Qiskrypt's Semi-Quantum Register.
+    Object Class of the Qiskrypt's Ancilla Semi-Quantum Register.
     """
 
-    def __init__(self, name="ancilla_semi_qu_reg", ancilla_num_qubits=1, ancilla_quantum_register=None):
+    def __init__(self, name="anc_semi_qu_reg", num_ancilla_qubits=1, ancilla_quantum_register=None):
         """
         Constructor for the Qiskrypt's Ancilla Semi-Quantum Register.
 
         It calls the constructor of the super-class Qiskrypt's Ancilla Quantum Register.
 
         :param name: The name of the Qiskrypt's Ancilla Semi-Quantum Register.
-        :param ancilla_num_qubits: The number of bits of the Qiskrypt's Ancilla Semi-Quantum Register.
+        :param num_ancilla_qubits: The number of ancilla qubits of the Qiskrypt's Ancilla Semi-Quantum Register.
         :param ancilla_quantum_register: A built-in ancilla quantum register object of
                                          the IBM's Qiskit Ancilla Quantum Register.
         """
-        super().__init__(name=name, num_ancilla_qubits=ancilla_num_qubits, ancilla_quantum_register=ancilla_quantum_register)
+        super().__init__(name=name, num_ancilla_qubits=num_ancilla_qubits, ancilla_quantum_register=ancilla_quantum_register)
         """
-        Calls the constructor of the super-class Qiskrypt's Quantum Register.
+        Calls the constructor of the super-class Qiskrypt's Ancilla Quantum Register.
         """
 
     def get_name(self):
         """
-        Return the name of the Qiskrypt's Semi-Quantum Register.
+        Return the name of the Qiskrypt's Ancilla Semi-Quantum Register.
 
-        :return super().get_name(): the name of the Qiskrypt's Semi-Quantum Register.
+        :return super().get_name(): the name of the Qiskrypt's Ancilla Semi-Quantum Register.
         """
 
         return super().get_name()
 
-    def get_num_qubits(self):
+    def get_num_ancilla_qubits(self):
         """
-        Return the number of qubits of the Qiskrypt's Semi-Quantum Register.
+        Return the number of ancilla qubits of the Qiskrypt's Ancilla Semi-Quantum Register.
 
-        :return super().get_num_qubits(): the number of qubits of
-                                          the Qiskrypt's Semi-Quantum Register.
-        """
-
-        return super().get_num_qubits()
-
-    def get_semi_quantum_register(self):
-        """
-        Return the IBM's Qiskit Quantum Register of the Qiskrypt's Semi-Quantum Register.
-
-        :return super().get_quantum_register(): the IBM's Qiskit Quantum Register of
-                                                the Qiskrypt's Semi-Quantum Register.
+        :return super().get_num_ancilla_qubits(): the number of ancilla qubits of
+                                                  the Qiskrypt's Ancilla Semi-Quantum Register.
         """
 
-        return super().get_quantum_register()
+        return super().get_num_ancilla_qubits()
+
+    def get_ancilla_semi_quantum_register(self):
+        """
+        Return the IBM's Qiskit Ancilla Quantum Register of the Qiskrypt's Ancilla Semi-Quantum Register.
+
+        :return super().get_ancilla_quantum_register(): the IBM's Qiskit Ancilla Quantum Register of
+                                                        the Qiskrypt's Ancilla Semi-Quantum Register.
+        """
+
+        return super().get_ancilla_quantum_register()
 
     @staticmethod
     def raise_unsupported_operation_error():
         """
-        Return/Raise an Unsupported Operation Error for the Qiskrypt's Semi-Quantum Register.
+        Return/Raise an Unsupported Operation Error for the Qiskrypt's Ancilla Semi-Quantum Register.
 
         :raise unsupported_operation_error: an Unsupported Operation Error for
-                                            the Qiskrypt's Semi-Quantum Register.
+                                            the Qiskrypt's Ancilla Semi-Quantum Register.
         """
 
-        unsupported_operation_error = QiskryptSemiQuantumRegisterUnsupportedOperationError()
+        unsupported_operation_error = QiskryptAncillaSemiQuantumRegisterUnsupportedOperationError()
         """
-        Retrieve the Unsupported Operation Error for the Qiskrypt's Semi-Quantum Register.
+        Retrieve the Unsupported Operation Error for the Qiskrypt's Ancilla Semi-Quantum Register.
         """
 
         """
-        Raise the Unsupported Operation Error for the Qiskrypt's Semi-Quantum Register.
+        Raise the Unsupported Operation Error for the Qiskrypt's Ancilla Semi-Quantum Register.
         """
         raise unsupported_operation_error
 
     @staticmethod
-    def raise_not_semi_quantum_register_error():
+    def raise_not_ancilla_semi_quantum_register_error():
         """
-        Return/Raise a Not a Semi-Quantum Register Error for the Qiskrypt's Semi-Quantum Register.
-        :raise not_semi_quantum_register_error: a Not a Semi-Quantum Register Error for the Qiskrypt's Semi-Quantum Register.
-        """
-
-        not_semi_quantum_register_error = QiskryptNotSemiQuantumRegisterError()
-        """
-        Retrieve the Not a Semi-Quantum Register Error for the Qiskrypt's Semi-Quantum Register.
+        Return/Raise a Not an Ancilla Semi-Quantum Register Error for the Qiskrypt's Ancilla Semi-Quantum Register.
+        :raise not_ancilla_semi_quantum_register_error: a Not an Ancilla Semi-Quantum Register Error for
+                                                        the Qiskrypt's Ancilla Semi-Quantum Register.
         """
 
+        not_ancilla_semi_quantum_register_error = QiskryptNotAncillaSemiQuantumRegisterError()
         """
-        Raise the Not a Semi-Quantum Register Error for the Qiskrypt's Semi-Quantum Register.
+        Retrieve the Not an Ancilla Semi-Quantum Register Error for the Qiskrypt's Ancilla Semi-Quantum Register.
         """
-        raise not_semi_quantum_register_error
+
+        """
+        Raise the Not an Ancilla Semi-Quantum Register Error for the Qiskrypt's Ancilla Semi-Quantum Register.
+        """
+        raise not_ancilla_semi_quantum_register_error
