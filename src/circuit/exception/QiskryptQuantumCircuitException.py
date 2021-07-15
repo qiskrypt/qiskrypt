2cd33d73c8f613a65c1410ddc3706e8ff3f37749
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-15 17:16:39 +0100

    More Exceptions for the Qiskrypt's Quantum Circuit added

--- src/circuit/exception/QiskryptQuantumCircuitException.py
+++ src/circuit/exception/QiskryptQuantumCircuitException.py
@@ -46,14 +46,32 @@ the Qiskrypt's Quantum Circuit.
 
 MESSAGE_INVALID_QISKIT_QUANTUM_REGISTER_INDEX_GIVEN_EXCEPTION = "Invalid IBM Qiskit's Quantum Register Index Given for the Qiskrypt's Quantum Circuit!!!\n"
 """
-The custom defined message for the Invalid IBM Qiskit's Quantum Register Index Given for
+The custom defined message for the Invalid IBM Qiskit's Quantum Register Index Given Error for
 the Qiskrypt's Quantum Circuit.
 """
 
 MESSAGE_INVALID_QUBIT_INDEX_GIVEN_EXCEPTION = "Invalid Qubit Index Given for the IBM Qiskit's Quantum Register!!!\n"
 """
-The custom defined message for the Invalid Qubit Index Given for
+The custom defined message for the Invalid Qubit Index Given Error for
 the IBM Qiskit's Quantum Register.
+"""
+
+MESSAGE_INVALID_QISKIT_CLASSICAL_REGISTER_INDEX_GIVEN_EXCEPTION = "Invalid IBM Qiskit's Classical Register Index Given for the Qiskrypt's Quantum Circuit!!!\n"
+"""
+The custom defined message for the Invalid IBM Qiskit's Classical Register Index Given Error for
+the Qiskrypt's Quantum Circuit.
+"""
+
+MESSAGE_INVALID_BIT_INDEX_GIVEN_EXCEPTION = "Invalid Qubit Index Given for the IBM Qiskit's Classical Register!!!\n"
+"""
+The custom defined message for the Invalid Qubit Index Given Error for
+the IBM Qiskit's Classical Register.
+"""
+
+MESSAGE_REGISTER_NOT_FOUND_EXCEPTION = "The supposed Qiskrypt's Register do not exist in the Qiskrypt's Quantum Circuit!!!\n"
+"""
+The custom defined message for the Qiskrypt's Register Not Found Error for
+the Qiskrypt's Quantum Circuit.
 """
 
 
@@ -67,6 +85,7 @@ class QiskryptQuantumCircuitUnsupportedTypeRegistersError(Exception):
         """
         Constructor for the Unsupported Type of Registers Error for
         the Qiskrypt's Quantum Circuit.
+
         :param message: The custom message for the Unsupported Type of Registers Error for
                         the Qiskrypt's Quantum Circuit.
         """
@@ -91,16 +110,17 @@ class QiskryptQuantumCircuitInvalidQiskitQuantumRegisterIndexGivenError(Exceptio
 
     def __init__(self, message=MESSAGE_INVALID_QISKIT_QUANTUM_REGISTER_INDEX_GIVEN_EXCEPTION):
         """
-        Constructor for the Invalid Qiskit Quantum Register Index Given Error for
-        the Qiskrypt's Quantum Circuit.
-        :param message: The custom message for the Invalid Qiskit Quantum Register Index Given Error for
-                        the Qiskrypt's Quantum Circuit.
+        Constructor for the Invalid Qiskit
+        Quantum Register Index Given Error for the Qiskrypt's Quantum Circuit.
+
+        :param message: The custom message for the Invalid Qiskit
+                        Quantum Register Index Given Error for the Qiskrypt's Quantum Circuit.
         """
 
         self.message = message
         """
-        Set the custom message for the Invalid Qiskit Quantum Register Index Given Error for
-        the Qiskrypt's Quantum Circuit.
+        Set the custom message for the Invalid Qiskit
+        Quantum Register Index Given Error for the Qiskrypt's Quantum Circuit.
         """
 
         super().__init__(self.message)
@@ -119,6 +139,7 @@ class QiskryptQuantumCircuitInvalidQubitIndexGivenError(Exception):
         """
         Constructor for the Invalid Qubit Index Given Error for
         the Qiskrypt's Quantum Circuit.
+
         :param message: The custom message for the Invalid Qubit Index Given Error for
                         the Qiskrypt's Quantum Circuit.
         """
@@ -126,6 +147,87 @@ class QiskryptQuantumCircuitInvalidQubitIndexGivenError(Exception):
         self.message = message
         """
         Set the custom message for the Invalid Qubit Index Given Error for
+        the Qiskrypt's Quantum Circuit.
+        """
+
+        super().__init__(self.message)
+        """
+        Calls the constructor of the super-class Exception.
+        """
+
+
+class QiskryptQuantumCircuitInvalidQiskitClassicalRegisterIndexGivenError(Exception):
+    """
+    Object Class of the Invalid Qiskit Classical Register Index Given Error for
+    the Qiskrypt's Quantum Circuit.
+    """
+
+    def __init__(self, message=MESSAGE_INVALID_QISKIT_CLASSICAL_REGISTER_INDEX_GIVEN_EXCEPTION):
+        """
+        Constructor for the Invalid Qiskit
+        Classical Register Index Given Error for the Qiskrypt's Quantum Circuit.
+
+        :param message: The custom message for the Invalid Qiskit
+                        Classical Register Index Given Error for the Qiskrypt's Quantum Circuit.
+        """
+
+        self.message = message
+        """
+        Set the custom message for the Invalid Qiskit
+        Classical Register Index Given Error for the Qiskrypt's Quantum Circuit.
+        """
+
+        super().__init__(self.message)
+        """
+        Calls the constructor of the super-class Exception.
+        """
+
+
+class QiskryptQuantumCircuitInvalidBitIndexGivenError(Exception):
+    """
+    Object Class of the Invalid Bit Index Given Error for
+    the Qiskrypt's Quantum Circuit.
+    """
+
+    def __init__(self, message=MESSAGE_INVALID_BIT_INDEX_GIVEN_EXCEPTION):
+        """
+        Constructor for the Invalid Bit Index Given Error for
+        the Qiskrypt's Quantum Circuit.
+
+        :param message: The custom message for the Invalid Bit Index Given Error for
+                        the Qiskrypt's Quantum Circuit.
+        """
+
+        self.message = message
+        """
+        Set the custom message for the Invalid Bit Index Given Error for
+        the Qiskrypt's Quantum Circuit.
+        """
+
+        super().__init__(self.message)
+        """
+        Calls the constructor of the super-class Exception.
+        """
+
+
+class QiskryptQuantumCircuitRegisterNotFoundError(Exception):
+    """
+    Object Class of the Register Not Found Error for
+    the Qiskrypt's Quantum Circuit.
+    """
+
+    def __init__(self, message=MESSAGE_REGISTER_NOT_FOUND_EXCEPTION):
+        """
+        Constructor for the Register Not Found Error for
+        the Qiskrypt's Quantum Circuit.
+
+        :param message: The custom message for the Register Not Found Error for
+                        the Qiskrypt's Quantum Circuit.
+        """
+
+        self.message = message
+        """
+        Set the custom message for the Register Not Found Error for
         the Qiskrypt's Quantum Circuit.
         """
 
