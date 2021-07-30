3bfc9b7e62bc5358701ba8e8a60bad426ab112be
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-07-30 22:12:50 +0100

    More Unitary Tests added to the Qiskrypt's Quantum Dice Throwing

--- test/true_random/dice_throwing/QiskryptQuantumDiceThrowingTest.py
+++ test/true_random/dice_throwing/QiskryptQuantumDiceThrowingTest.py
@@ -73,7 +73,7 @@ class QiskryptQuantumDiceThrowingTests(TestCase):
         """
         Test Case #1:
 
-        - Initialise the Qiskrypt's Quantum Dice Throwing, for a Tetrahedron Dice.
+        - Throw a Tetrahedron Dice from the Qiskrypt's Quantum Dice Throwing.
 
         Description of the Steps for the Unitary Test:
         1) The Qiskrypt's Quantum Dice Throwing is configured, for a Tetrahedron Dice;
@@ -90,7 +90,8 @@ class QiskryptQuantumDiceThrowingTests(TestCase):
 
         qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[0])
         """
-        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing.
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for a Tetrahedron Dice.
         """
 
         qiskrypt_quantum_dice_throwing.throw_dice()
@@ -108,6 +109,261 @@ class QiskryptQuantumDiceThrowingTests(TestCase):
         print(f"\n\nTest #1:"
               f"\n - The number of points extracted from "
               f"throwing a Tetrahedron Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
+              f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
+        """
+        Print the resulted number of points, extracted from
+        the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        """
+        Dummy Assert Equal for the Unittest.
+        """
+        self.assertEqual(True, True)
+
+    def test_no_2_qiskrypt_quantum_dice_throwing_cube(self):
+        """
+        Test Case #2:
+
+        - Throw a Cube Dice from the Qiskrypt's Quantum Dice Throwing.
+
+        Description of the Steps for the Unitary Test:
+        1) The Qiskrypt's Quantum Dice Throwing is configured, for a Cube Dice;
+        2) The Cube Dice is thrown from the Qiskrypt's Quantum Dice Throwing;
+
+        Return OK (or FAIL) if, all the Tests performed are OK (or FAIL, otherwise).
+        """
+
+        qiskrypt_quantum_dice_throwing = \
+            QiskryptQuantumDiceThrowing("qu_dice_throw_cube_2")
+        """
+        Create a Qiskrypt's Quantum Dice Throwing, for a Cube Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[1])
+        """
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for a Cube Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.throw_dice()
+        """
+        Throw the Cube Dice from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        num_points_on_dice = qiskrypt_quantum_dice_throwing.get_dice_throwing_num_points()
+        """
+        Extract the respective number of points on the dice,
+        from the resulted classical outcome/result,
+        after the Cube Dice been thrown from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        print(f"\n\nTest #2:"
+              f"\n - The number of points extracted from "
+              f"throwing a Cube Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
+              f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
+        """
+        Print the resulted number of points, extracted from
+        the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        """
+        Dummy Assert Equal for the Unittest.
+        """
+        self.assertEqual(True, True)
+
+    def test_no_3_qiskrypt_quantum_dice_throwing_octahedron(self):
+        """
+        Test Case #3:
+
+        - Throw an Octahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+
+        Description of the Steps for the Unitary Test:
+        1) The Qiskrypt's Quantum Dice Throwing is configured, for an Octahedron Dice;
+        2) The Octahedron Dice is thrown from the Qiskrypt's Quantum Dice Throwing;
+
+        Return OK (or FAIL) if, all the Tests performed are OK (or FAIL, otherwise).
+        """
+
+        qiskrypt_quantum_dice_throwing = \
+            QiskryptQuantumDiceThrowing("qu_dice_throw_octahedron_3")
+        """
+        Create a Qiskrypt's Quantum Dice Throwing, for an Octahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[2])
+        """
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for an Octahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.throw_dice()
+        """
+        Throw the Octahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        num_points_on_dice = qiskrypt_quantum_dice_throwing.get_dice_throwing_num_points()
+        """
+        Extract the respective number of points on the dice,
+        from the resulted classical outcome/result,
+        after the Octahedron Dice been thrown from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        print(f"\n\nTest #3:"
+              f"\n - The number of points extracted from "
+              f"throwing an Octahedron Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
+              f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
+        """
+        Print the resulted number of points, extracted from
+        the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        """
+        Dummy Assert Equal for the Unittest.
+        """
+        self.assertEqual(True, True)
+
+    def test_no_4_qiskrypt_quantum_dice_throwing_pentagonal_trapezohedron(self):
+        """
+        Test Case #4:
+
+        - Throw a Pentagonal Trapezohedron Dice from the Qiskrypt's Quantum Dice Throwing.
+
+        Description of the Steps for the Unitary Test:
+        1) The Qiskrypt's Quantum Dice Throwing is configured, for a Pentagonal Trapezohedron Dice;
+        2) The Pentagonal Trapezohedron Dice is thrown from the Qiskrypt's Quantum Dice Throwing;
+
+        Return OK (or FAIL) if, all the Tests performed are OK (or FAIL, otherwise).
+        """
+
+        qiskrypt_quantum_dice_throwing = \
+            QiskryptQuantumDiceThrowing("qu_dice_throw_pentagonal_trapezohedron_4")
+        """
+        Create a Qiskrypt's Quantum Dice Throwing, for a Pentagonal Trapezohedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[3])
+        """
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for a Pentagonal Trapezohedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.throw_dice()
+        """
+        Throw the Pentagonal Trapezohedron Dice from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        num_points_on_dice = qiskrypt_quantum_dice_throwing.get_dice_throwing_num_points()
+        """
+        Extract the respective number of points on the dice,
+        from the resulted classical outcome/result,
+        after the Pentagonal Trapezohedron Dice been thrown from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        print(f"\n\nTest #4:"
+              f"\n - The number of points extracted from "
+              f"throwing a Pentagonal Trapezohedron Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
+              f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
+        """
+        Print the resulted number of points, extracted from
+        the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        """
+        Dummy Assert Equal for the Unittest.
+        """
+        self.assertEqual(True, True)
+
+    def test_no_5_qiskrypt_quantum_dice_throwing_dodecahedron(self):
+        """
+        Test Case #5:
+
+        - Throw a Dodecahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+
+        Description of the Steps for the Unitary Test:
+        1) The Qiskrypt's Quantum Dice Throwing is configured, for a Dodecahedron Dice;
+        2) The Dodecahedron Dice is thrown from the Qiskrypt's Quantum Dice Throwing;
+
+        Return OK (or FAIL) if, all the Tests performed are OK (or FAIL, otherwise).
+        """
+
+        qiskrypt_quantum_dice_throwing = \
+            QiskryptQuantumDiceThrowing("qu_dice_throw_dodecahedron_5")
+        """
+        Create a Qiskrypt's Quantum Dice Throwing, for a Dodecahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[4])
+        """
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for a Dodecahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.throw_dice()
+        """
+        Throw the Dodecahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        num_points_on_dice = qiskrypt_quantum_dice_throwing.get_dice_throwing_num_points()
+        """
+        Extract the respective number of points on the dice,
+        from the resulted classical outcome/result,
+        after the Dodecahedron Dice been thrown from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        print(f"\n\nTest #5:"
+              f"\n - The number of points extracted from "
+              f"throwing a Dodecahedron Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
+              f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
+        """
+        Print the resulted number of points, extracted from
+        the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        """
+        Dummy Assert Equal for the Unittest.
+        """
+        self.assertEqual(True, True)
+
+    def test_no_6_qiskrypt_quantum_dice_throwing_icosahedron(self):
+        """
+        Test Case #6:
+
+        - Throw an Icosahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+
+        Description of the Steps for the Unitary Test:
+        1) The Qiskrypt's Quantum Dice Throwing is configured, for an Icosahedron Dice;
+        2) The Icosahedron Dice is thrown from the Qiskrypt's Quantum Dice Throwing;
+
+        Return OK (or FAIL) if, all the Tests performed are OK (or FAIL, otherwise).
+        """
+
+        qiskrypt_quantum_dice_throwing = \
+            QiskryptQuantumDiceThrowing("qu_dice_throw_icosahedron_6")
+        """
+        Create a Qiskrypt's Quantum Dice Throwing, for an Icosahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.configure(DICE_TYPES[5])
+        """
+        Initiate/Configure the previously created Qiskrypt's Quantum Dice Throwing,
+        for an Icosahedron Dice.
+        """
+
+        qiskrypt_quantum_dice_throwing.throw_dice()
+        """
+        Throw the Icosahedron Dice from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        num_points_on_dice = qiskrypt_quantum_dice_throwing.get_dice_throwing_num_points()
+        """
+        Extract the respective number of points on the dice,
+        from the resulted classical outcome/result,
+        after the Icosahedron Dice been thrown from the Qiskrypt's Quantum Dice Throwing.
+        """
+
+        print(f"\n\nTest #6:"
+              f"\n - The number of points extracted from "
+              f"throwing an Icosahedron Dice from the Qiskrypt's Quantum Dice Throwing is:\n"
               f"   [ {num_points_on_dice} ] (Decimal Format) ;\n")
         """
         Print the resulted number of points, extracted from
