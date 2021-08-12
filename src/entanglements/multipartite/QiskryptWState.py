2de11a33cd325f6b65a6ab18f710c8165cb9db23
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-08-12 17:06:54 +0100

    Methods added to the Qiskrypt's W State to prepare and measure its Quantum Entanglements in Computational and W State Bases

--- src/entanglements/multipartite/QiskryptWState.py
+++ src/entanglements/multipartite/QiskryptWState.py
@@ -67,6 +67,11 @@ from src.circuit.QiskryptQuantumCircuit \
 Import the Qiskrypt's Quantum Circuit.
 """
 
+from numpy import arccos, sqrt
+"""
+Import the Arc-Cosine and Squared Roots functions from Python's NumPy.
+"""
+
 
 class QiskryptWState(QiskryptQuantumEntanglement):
     """
@@ -212,3 +217,304 @@ class QiskryptWState(QiskryptQuantumEntanglement):
         Set the indexes of the qubits inside the IBM Qiskit's Quantum Registers,
         as the given indexes for it.
         """
+
+    def prepare_multipartite_entanglement_at_computational_basis(self, is_to_measure_at_computational_basis=False,
+                                                                 qiskit_classical_registers_indexes=None,
+                                                                 bits_indexes=None) -> None:
+        """
+        Prepare the Multipartite Quantum Entanglement,
+        for the specified Qiskrypt's W State, as a Quantum Entangled State,
+        or even, apply also the measurement of that Quantum Entangled State, on the Computational Basis,
+        to allow the extraction of its final classical outcome/result.
+
+        :param is_to_measure_at_computational_basis: the boolean flag to keep the information about
+                                                     if it will be performed a measurement of
+                                                     the Qiskrypt's W State on the Computational Basis.
+        :param qiskit_classical_registers_indexes: the indexes of the IBM Qiskit's Classical Registers,
+                                                   where it belongs the bits to store the final classical outcomes/results,
+                                                   after be performed a measurement on the qubits inside
+                                                   the IBM Qiskit's Quantum Registers.
+        :param bits_indexes: the indexes of the bits inside the IBM Qiskit's Classical Registers,
+                             where it will be stored the final classical outcomes/results,
+                             after be performed a measurement on the qubits inside
+                             the IBM Qiskit's Quantum Registers.
+        """
+
+        self.qiskrypt_quantum_circuit \
+            .apply_barriers_set_qubits_in_set_qiskit_quantum_registers(self.qiskit_quantum_registers_indexes,
+                                                                       self.qubits_indexes)
+        """
+        Apply barriers to the IBM Qiskit's Quantum Registers and the respective qubits.
+        """
+
+        num_quantum_registers = len(self.qiskit_quantum_registers_indexes)
+        """
+        Retrieve the number of the IBM Qiskit's Quantum Registers for the Qiskrypt's W State.
+        """
+
+        num_qubits = len(self.qubits_indexes)
+        """
+        Retrieve the number of qubits for the Qiskrypt's W State.
+        """
+
+        self.qiskrypt_quantum_circuit.apply_pauli_x(self.qiskit_quantum_registers_indexes[(num_quantum_registers - 1)],
+                                                    self.qubits_indexes[(num_qubits - 1)])
+        """
+        Apply the Pauli-X Gate/Operation to the given indexes of
+        the last IBM Qiskit's Quantum Register and the respective qubit on it.
+        """
+
+        for qiskit_quantum_register_index, qubit_index in \
+                zip(range((num_quantum_registers - 1)), range((num_qubits - 1))):
+            """
+            For each pair of indexes for an IBM Qiskit's Quantum Register and a qubit. 
+            """
+
+            quantum_register_operator_index = (num_quantum_registers - qiskit_quantum_register_index)
+            """
+            Compute the operator index for the IBM Qiskit's Quantum Register.
+            """
+
+            qubit_operator_index = (num_qubits - qubit_index)
+            """
+            Compute the operator index for the qubit inside the IBM Qiskit's Quantum Register.
+            """
+
+            theta_radians = arccos(sqrt(1. / qubit_operator_index))
+            """
+            Compute the theta angle, in radians, from the Arc-Cosine function of
+            the Square Root of the inverse of the operator index for the qubit inside
+            the IBM Qiskit's Quantum Register.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_ry_radians(-theta_radians,
+                                                           (quantum_register_operator_index - 2),
+                                                           (qubit_operator_index - 2))
+            """
+            Apply the Rotation-Y (R_y(-theta_radians)) Gate/Operation, with the symmetric theta angle in radians,
+            to the given indexes of the current operator index for the IBM Qiskit's Quantum Register and
+            the current operator index for the qubit on it.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_controlled_pauli_z((quantum_register_operator_index - 1),
+                                                                   (quantum_register_operator_index - 2),
+                                                                   (qubit_operator_index - 1),
+                                                                   (qubit_operator_index - 2))
+            """
+            Apply the Controlled-Pauli-Z (Controlled-Phase-Flip/Controlled-Phase-Shifter) Gate/Operation to
+            the given indexes of the control IBM Qiskit's Quantum Register and the respective qubit on it, as also,
+            the current target IBM Qiskit's Quantum Register and the current respective qubit on it.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_ry_radians(theta_radians,
+                                                           (quantum_register_operator_index - 2),
+                                                           (qubit_operator_index - 2))
+            """
+            Apply again the Rotation-Y (R_y(theta_radians)) Gate/Operation, with the regular theta angle in radians,
+            to the given indexes of the current operator index for the IBM Qiskit's Quantum Register and
+            the current operator index for the qubit on it.
+            """
+
+        for qiskit_quantum_register_index, qubit_index in \
+                zip(range((num_quantum_registers - 1)), range((num_qubits - 1))):
+            """
+            For each pair of indexes for an IBM Qiskit's Quantum Register and a qubit. 
+            """
+
+            quantum_register_operator_index = (num_quantum_registers - qiskit_quantum_register_index)
+            """
+            Compute the operator index for the IBM Qiskit's Quantum Register.
+            """
+
+            qubit_operator_index = (num_qubits - qubit_index)
+            """
+            Compute the operator index for the qubit inside the IBM Qiskit's Quantum Register.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_controlled_pauli_x((quantum_register_operator_index - 2),
+                                                                   (quantum_register_operator_index - 1),
+                                                                   (qubit_operator_index - 2),
+                                                                   (qubit_operator_index - 1))
+            """
+            Apply the Controlled-Pauli-X (Controlled-NOT) Gate/Operation to
+            the given indexes of the control IBM Qiskit's Quantum Register and the respective qubit on it, as also,
+            the current target IBM Qiskit's Quantum Register and the current respective qubit on it.
+            """
+
+        self.qiskrypt_quantum_circuit \
+            .apply_barriers_set_qubits_in_set_qiskit_quantum_registers(self.qiskit_quantum_registers_indexes,
+                                                                       self.qubits_indexes)
+        """
+        Apply barriers to the IBM Qiskit's Quantum Registers and the respective qubits.
+        """
+
+        if is_to_measure_at_computational_basis:
+            """
+            If the boolean flag to keep the information about
+            if it will be performed a measurement of
+            the Qiskrypt's W State on the Computational Basis, is True.
+            """
+
+            if self.qiskrypt_quantum_circuit.get_total_num_bits() >= \
+                    len(qiskit_classical_registers_indexes):
+                """
+                If the number of bits of
+                the given Qiskrypt's Quantum Circuit is greater or equal than
+                the number of the IBM Qiskit's Classical Registers (and the respective bits).
+                """
+
+                self.qiskrypt_quantum_circuit\
+                    .measure_set_qubits_in_set_qiskit_quantum_registers(self.qiskit_quantum_registers_indexes,
+                                                                        qiskit_classical_registers_indexes,
+                                                                        self.qubits_indexes,
+                                                                        bits_indexes)
+                """
+                Measure the qubits in the IBM Qiskit's Quantum Registers into
+                the respective bits in the IBM Qiskit's Classical Registers,
+                inside the IBM Qiskit's Quantum Circuit of the Qiskrypt's Quantum Circuit.
+                """
+
+            else:
+                """
+                If the number of bits of the given Qiskrypt's Quantum Circuit is lower than
+                the number of the IBM Qiskit's Classical Registers (and the respective bits).
+                """
+
+                # TODO - Throw Exception
+
+    def prepare_multipartite_entanglement_at_w_state_basis(self, is_to_measure_at_w_state_basis=False,
+                                                           qiskit_classical_registers_indexes=None,
+                                                           bits_indexes=None) -> None:
+        """
+        Prepare the Multipartite Quantum Entanglement,
+        for the specified Qiskrypt's W State, as a Quantum Entangled State,
+        or even, apply also the measurement of that Quantum Entangled State, on the W State Basis,
+        to allow the extraction of its final classical outcome/result.
+
+        :param is_to_measure_at_w_state_basis: the boolean flag to keep the information about
+                                               if it will be performed a measurement of
+                                               the Qiskrypt's W State on the W State Basis.
+        :param qiskit_classical_registers_indexes: the indexes of the IBM Qiskit's Classical Registers,
+                                                   where it belongs the bits to store the final classical outcomes/results,
+                                                   after be performed a measurement on the qubits inside
+                                                   the IBM Qiskit's Quantum Registers.
+        :param bits_indexes: the indexes of the bits inside the IBM Qiskit's Classical Registers,
+                             where it will be stored the final classical outcomes/results,
+                             after be performed a measurement on the qubits inside
+                             the IBM Qiskit's Quantum Registers.
+        """
+
+        self.qiskrypt_quantum_circuit \
+            .apply_barriers_set_qubits_in_set_qiskit_quantum_registers(self.qiskit_quantum_registers_indexes,
+                                                                       self.qubits_indexes)
+        """
+        Apply barriers to the IBM Qiskit's Quantum Registers and the respective qubits.
+        """
+
+        num_quantum_registers = len(self.qiskit_quantum_registers_indexes)
+        """
+        Retrieve the number of the IBM Qiskit's Quantum Registers for the Qiskrypt's W State.
+        """
+
+        num_qubits = len(self.qubits_indexes)
+        """
+        Retrieve the number of qubits for the Qiskrypt's W State.
+        """
+
+        for qiskit_quantum_register_index, qubit_index in \
+                zip(range((num_quantum_registers - 1)), range((num_qubits - 1))):
+            """
+            For each pair of indexes for an IBM Qiskit's Quantum Register and a qubit. 
+            """
+
+            self.qiskrypt_quantum_circuit.apply_controlled_pauli_x(qiskit_quantum_register_index,
+                                                                   (qiskit_quantum_register_index + 1),
+                                                                   qubit_index,
+                                                                   (qubit_index + 1))
+            """
+            Apply the Controlled-Pauli-X (Controlled-NOT) Gate/Operation to
+            the given indexes of the control IBM Qiskit's Quantum Register and the respective qubit on it, as also,
+            the current target IBM Qiskit's Quantum Register and the current respective qubit on it.
+            """
+
+        for qiskit_quantum_register_index, qubit_index in \
+                zip(range((num_quantum_registers - 1)), range((num_qubits - 1))):
+            """
+            For each pair of indexes for an IBM Qiskit's Quantum Register and a qubit. 
+            """
+
+            theta_radians = arccos(sqrt(1. / (qubit_index + 2)))
+            """
+            Compute the theta angle, in radians, from the Arc-Cosine function of
+            the Square Root of the inverse of the index of the qubit inside
+            the IBM Qiskit's Quantum Register.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_ry_radians(-theta_radians,
+                                                           (qiskit_quantum_register_index + 1),
+                                                           qubit_index)
+            """
+            Apply the Rotation-Y (R_y(-theta_radians)) Gate/Operation, with the symmetric theta angle in radians,
+            to the given indexes of the current index for the IBM Qiskit's Quantum Register and
+            the current index for the qubit on it.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_controlled_pauli_z((qiskit_quantum_register_index + 1),
+                                                                   qiskit_quantum_register_index,
+                                                                   (qubit_index + 1),
+                                                                   qubit_index)
+            """
+            Apply the Controlled-Pauli-Z (Controlled-Phase-Flip/Controlled-Phase-Shifter) Gate/Operation to
+            the given indexes of the control IBM Qiskit's Quantum Register and the respective qubit on it, as also,
+            the current target IBM Qiskit's Quantum Register and the current respective qubit on it.
+            """
+
+            self.qiskrypt_quantum_circuit.apply_ry_radians(theta_radians,
+                                                           (qiskit_quantum_register_index + 1),
+                                                           qubit_index)
+            """
+            Apply the Rotation-Y (R_y(-theta_radians)) Gate/Operation, with the symmetric theta angle in radians,
+            to the given indexes of the current index for the IBM Qiskit's Quantum Register and
+            the current index for the qubit on it.
+            """
+
+        self.qiskrypt_quantum_circuit.apply_pauli_x(self.qiskit_quantum_registers_indexes[(num_quantum_registers - 1)],
+                                                    self.qubits_indexes[(num_qubits - 1)])
+        """
+        Apply the Pauli-X Gate/Operation to the given indexes of
+        the last IBM Qiskit's Quantum Register and the respective qubit on it.
+        """
+
+        if is_to_measure_at_w_state_basis:
+            """
+            If the boolean flag to keep the information about
+            if it will be performed a measurement of
+            the Qiskrypt's W State on the W State Basis, is True.
+            """
+
+            if self.qiskrypt_quantum_circuit.get_total_num_bits() >= \
+                    len(qiskit_classical_registers_indexes):
+                """
+                If the number of bits of
+                the given Qiskrypt's Quantum Circuit is greater or equal than
+                the number of the IBM Qiskit's Classical Registers (and the respective bits).
+                """
+
+                self.qiskrypt_quantum_circuit\
+                    .measure_set_qubits_in_set_qiskit_quantum_registers(self.qiskit_quantum_registers_indexes,
+                                                                        qiskit_classical_registers_indexes,
+                                                                        self.qubits_indexes,
+                                                                        bits_indexes)
+                """
+                Measure the qubits in the IBM Qiskit's Quantum Registers into
+                the respective bits in the IBM Qiskit's Classical Registers,
+                inside the IBM Qiskit's Quantum Circuit of the Qiskrypt's Quantum Circuit.
+                """
+
+            else:
+                """
+                If the number of bits of the given Qiskrypt's Quantum Circuit is lower than
+                the number of the IBM Qiskit's Classical Registers (and the respective bits).
+                """
+
+                # TODO - Throw Exception
