03d454f25f1b2c647954c7716b56b0fb2112c16c
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2022-01-25 16:33:04 +0000

    Some minor changes added to the Qiskrypt's Classical Utilities

--- src/classical_regime/common/QiskryptClassicalUtilities.py
+++ src/classical_regime/common/QiskryptClassicalUtilities.py
@@ -47,6 +47,30 @@ BINARY_FORMAT_START_OFFSET = 2
 The offset for the start of the Python's binary format.
 """
 
+INTEGER_BASE_2 = 2
+"""
+The parameter for the integers base-2.
+"""
+
+SIZE_BYTE_IN_NUM_BITS = 8
+"""
+The size of a byte in number of bits.
+"""
+
+BIG_ENDIAN_FORMAT_TAG = "big"
+"""
+The tag for the Big-Endian Format
+(the memory/register stores from the most-significant bytes to
+ the least-significant bytes).
+"""
+
+LITTLE_ENDIAN_FORMAT_TAG = "little"
+"""
+The tag for the Little-Endian Format
+(the memory/register stores from the least-significant bytes to
+ the most-significant bytes).
+"""
+
 
 class QiskryptClassicalUtilities:
     """
@@ -54,7 +78,7 @@ class QiskryptClassicalUtilities:
     """
 
     @staticmethod
-    def compute_hamming_weight(binary_string_bits: bin):
+    def compute_hamming_weight(binary_string_bits: bin) -> int:
         """
         Return the Hamming Weight of the bits of a given binary string.
 
@@ -70,7 +94,7 @@ class QiskryptClassicalUtilities:
 
         for current_bit in range(len(binary_string_bits[BINARY_FORMAT_START_OFFSET:])):
             """
-            For each bit of the of the given binary string.
+            For each bit of the of the given binary string, without the start offset.
             """
 
             if binary_string_bits[(BINARY_FORMAT_START_OFFSET + current_bit)] == "1":
@@ -87,3 +111,46 @@ class QiskryptClassicalUtilities:
         Return the Hamming Weight of the bits of a given binary string.
         """
         return hamming_weight
+
+    @staticmethod
+    def convert_binary_string_to_bytes(binary_string_bits: bin,
+                                       byte_order_format=BIG_ENDIAN_FORMAT_TAG) -> bytes:
+        """
+        Return the bytes of a given binary string in bits.
+
+        :param binary_string_bits: the bits of a given binary string in bits.
+        :param byte_order_format: the tag for the Endianness Format (and respective byte order).
+
+        :return: the bytes of a given binary string in bits.
+        """
+
+        binary_string_bits_without_start_offset = \
+            binary_string_bits[BINARY_FORMAT_START_OFFSET:]
+        """
+        Compute the given binary string in bits, without the start offset.
+        """
+
+        binary_string_bits_without_start_offset_length = \
+            len(binary_string_bits_without_start_offset)
+        """
+        Compute the given binary string in bits, without the start offset.
+        """
+
+        binary_string_integer_format = int(binary_string_bits_without_start_offset,
+                                           INTEGER_BASE_2)
+        """
+        Convert the given binary string in bits to an integer base-2 format.
+        """
+
+        binary_string_bytes = binary_string_integer_format\
+            .to_bytes(((binary_string_bits_without_start_offset_length +
+                        (SIZE_BYTE_IN_NUM_BITS - 1)) // SIZE_BYTE_IN_NUM_BITS),
+                      byteorder=byte_order_format)
+        """
+        Convert the given binary string in an integer base-2 format to bytes.
+        """
+
+        """
+        Return the bytes of a given binary string in bits.
+        """
+        return binary_string_bytes
