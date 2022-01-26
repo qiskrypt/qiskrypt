3b3ad81576688f08d1d0de656de7a762d0c8b3d9
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2022-01-26 12:57:18 +0000

    Some minor improvements added to the Qiskrypt's Communication Channel

--- src/quantum_regime/networking_and_communications/communication_channel/QiskryptCommunicationChannel.py
+++ src/quantum_regime/networking_and_communications/communication_channel/QiskryptCommunicationChannel.py
@@ -48,18 +48,40 @@ POSSIBLE_COMMUNICATION_CHANNELS_CONTEXTS = ["QUANTUM", "CLASSICAL"]
 The available Communication Channel contexts for the Qiskrypt's Communication Channel.
 """
 
+POSSIBLE_COMMUNICATION_CHANNELS_SCENARIOS = ["NOISELESS", "NOISY"]
+"""
+The available Communication Channel scenarios for the Qiskrypt's Communication Channel.
+"""
+
+POSSIBLE_COMMUNICATION_CHANNELS_MEDIUMS = ["FIBER-OPTIC", "SATELLITE"]
+"""
+The available Communication Channel scenarios for the Qiskrypt's Communication Channel.
+"""
+
+FIBER_OPTICS_MAX_DISTANCE_KMS_COHERENT_COMMUNICATIONS = 100.0
+"""
+The maximum distance in KMs (Kilometers) in Fiber Optics, for coherent Communications.
+"""
+
 
 class QiskryptCommunicationChannel:
     """
     Object class for the Qiskrypt's Communication Channel.
     """
 
-    def __init__(self, communication_channel_num: int, communication_channel_name: str):
+    def __init__(self, communication_channel_num: int, communication_channel_name: str,
+                 communication_channel_context: str, communication_channel_scenario: str,
+                 communication_channel_medium: str, communication_channel_distance_in_kms: float):
         """
         Constructor of the Qiskrypt's Communication Channel.
 
         :param communication_channel_num: the number of the Qiskrypt's Communication Channel.
         :param communication_channel_name: the name of the Qiskrypt's Communication Channel.
+        :param communication_channel_context: the context of the Qiskrypt's Communication Channel.
+        :param communication_channel_scenario: the scenario of the Qiskrypt's Communication Channel.
+        :param communication_channel_medium: the medium of the Qiskrypt's Communication Channel.
+        :param communication_channel_distance_in_kms: the distance of the Qiskrypt's Communication Channel,
+                                                      in KMs (Kilometers).
         """
 
         self.communication_channel_num = communication_channel_num
@@ -70,6 +92,26 @@ class QiskryptCommunicationChannel:
         self.communication_channel_name = communication_channel_name
         """
         Set the name of the Qiskrypt's Communication Channel.
+        """
+
+        self.communication_channel_context = communication_channel_context
+        """
+        Set the context of the Qiskrypt's Communication Channel.
+        """
+
+        self.communication_channel_scenario = communication_channel_scenario
+        """
+        Set the scenario of the Qiskrypt's Communication Channel.
+        """
+
+        self.communication_channel_medium = communication_channel_medium
+        """
+        Set the medium of the Qiskrypt's Communication Channel.
+        """
+
+        self.communication_channel_distance_in_kms = communication_channel_distance_in_kms
+        """
+        Set the distance of the Qiskrypt's Communication Channel, in KMs (Kilometers).
         """
 
     def get_communication_channel_num(self) -> int:
@@ -95,3 +137,52 @@ class QiskryptCommunicationChannel:
         Return the name of the Qiskrypt's Communication Channel.
         """
         return self.communication_channel_name
+
+    def get_communication_channel_context(self) -> str:
+        """
+        Return the context of the Qiskrypt's Communication Channel.
+
+        :return self.communication_channel_context: the context of the Qiskrypt's Communication Channel.
+        """
+
+        """
+        Return the context of the Qiskrypt's Communication Channel.
+        """
+        return self.communication_channel_context
+
+    def get_communication_channel_scenario(self) -> str:
+        """
+        Return the scenario of the Qiskrypt's Communication Channel.
+
+        :return self.communication_channel_scenario: the scenario of the Qiskrypt's Communication Channel.
+        """
+
+        """
+        Return the scenario of the Qiskrypt's Communication Channel.
+        """
+        return self.communication_channel_scenario
+
+    def get_communication_channel_medium(self) -> str:
+        """
+        Return the medium of the Qiskrypt's Communication Channel.
+
+        :return self.communication_channel_medium: the medium of the Qiskrypt's Communication Channel.
+        """
+
+        """
+        Return the medium of the Qiskrypt's Communication Channel.
+        """
+        return self.communication_channel_medium
+
+    def get_communication_channel_distance_in_kms(self) -> float:
+        """
+        Return the distance of the Qiskrypt's Communication Channel, in KMs (Kilometers).
+
+        :return self.communication_channel_distance_in_kms: the distance of the Qiskrypt's Communication Channel,
+                                                            in KMs (Kilometers).
+        """
+
+        """
+        Return the distance of the Qiskrypt's Communication Channel, in KMs (Kilometers).
+        """
+        return self.communication_channel_distance_in_kms
