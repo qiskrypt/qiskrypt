55d935f8045bff8c5e9b1857373f8fc9ebc33baf
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2022-02-09 13:25:42 +0000

    Several changes and improvements added to the Qiskrypt's Noiseless DV (Discrete Variables) BB84 Protocol Round

--- src/quantum_regime/cryptography/key_exchange/quantum_key_distribution/discrete_variables/prepare_and_measure/bb84/common/QiskryptDVBB84ProtocolRound.py
+++ src/quantum_regime/cryptography/key_exchange/quantum_key_distribution/discrete_variables/prepare_and_measure/bb84/common/QiskryptDVBB84ProtocolRound.py
@@ -73,33 +73,39 @@ class QiskryptDVBB84ProtocolRound(QiskryptQuantumKeyDistributionRound):
     Object class for the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round.
     """
 
-    def __init__(self, quantum_key_exchange_protocol_round_number,
-                 quantum_key_exchange_protocol_round_type,
-                 quantum_key_exchange_protocol_round_quantum_circuit):
+    def __init__(self, round_number: int, round_type: str,
+                 round_quantum_circuit: QiskryptQuantumCircuit):
         """
-        Constructor of the Qiskrypt's BB84 Protocol Round with Discrete Variables (DVs).
+        Constructor of the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round.
 
-        :param quantum_key_exchange_protocol_round_number: the number of the Qiskrypt's
-                                                           Quantum Key Exchange Protocol Round.
-        :param quantum_key_exchange_protocol_round_type: the type of the Qiskrypt's
-                                                         Quantum Key Exchange Protocol Round.
-        :param: quantum_key_exchange_protocol_round_quantum_circuit: the Qiskrypt's Quantum Circuit for
-                                                                     the Qiskrypt's Quantum Key Exchange
-                                                                     Protocol Round.
+        :param round_number: the number of the Qiskrypt's Quantum Key Exchange Protocol Round.
+        :param round_type: the type of the Qiskrypt's Quantum Key Exchange Protocol Round.
+        :param round_quantum_circuit: the Qiskrypt's Quantum Circuit for
+                                      the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
 
-        if quantum_key_exchange_protocol_round_type in POSSIBLE_DV_BB84_PROTOCOL_ROUND_TYPES:
+        if round_type in POSSIBLE_DV_BB84_PROTOCOL_ROUND_TYPES:
             """
             If the given type of the Qiskrypt's Quantum Key Exchange Protocol Round is
             one of the available (and thus, valid) BB84 Protocol Round types for
             the Qiskrypt's BB84 Protocol with Discrete Variables (DVs).
             """
 
-            super().__init__(quantum_key_exchange_protocol_round_number,
-                             quantum_key_exchange_protocol_round_type,
-                             quantum_key_exchange_protocol_round_quantum_circuit)
+            super().__init__(round_number, round_type, round_quantum_circuit)
             """
             Call of the constructor of the super-class Qiskrypt's Quantum Key Distribution (QKD) Round.
+            """
+
+            self.round_basis_sender_choice = None
+            """
+            Set the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, initially, as None.
+            """
+
+            self.round_basis_receiver_choice = None
+            """
+            Set the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, initially, as None.
             """
 
         else:
@@ -111,42 +117,216 @@ class QiskryptDVBB84ProtocolRound(QiskryptQuantumKeyDistributionRound):
 
             # TODO Throw - Exception
 
-    def get_quantum_key_exchange_protocol_round_number(self) -> int:
+    def get_round_number(self) -> int:
         """
         Return the number of the Qiskrypt's Quantum Key Distribution (QKD) Round.
 
-        :return super().get_quantum_key_exchange_protocol_round_number(): the number of the Qiskrypt's
-                                                                          Quantum Key Distribution Round.
+        :return super().get_round_number(): the number of the Qiskrypt's Quantum Key Distribution Round.
         """
 
         """
         Return the number of the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
-        return super().get_quantum_key_exchange_protocol_round_number()
+        return super().get_round_number()
 
-    def get_quantum_key_exchange_protocol_round_type(self) -> str:
+    def get_round_type(self) -> str:
         """
         Return the type of the Qiskrypt's Quantum Key Exchange Protocol Round.
 
-        :return super().get_quantum_key_exchange_protocol_round_type(): the type of the Qiskrypt's
-                                                                        Quantum Key Exchange Protocol Round.
+        :return super().get_round_type(): the type of the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
 
         """
         Return the type of the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
-        return super().get_quantum_key_exchange_protocol_round_type()
+        return super().get_round_type()
 
-    def get_quantum_key_exchange_protocol_round_quantum_circuit(self) -> QiskryptQuantumCircuit:
+    def get_round_quantum_circuit(self) -> QiskryptQuantumCircuit:
         """
         Return the Qiskrypt's Quantum Circuit of the Qiskrypt's Quantum Key Exchange Protocol Round.
 
-        :return super().get_quantum_key_exchange_protocol_round_quantum_circuit(): the Qiskrypt's Quantum Circuit of
-                                                                                   the Qiskrypt's Quantum Key Exchange
-                                                                                   Protocol Round.
+        :return super().get_round_quantum_circuit(): the Qiskrypt's Quantum Circuit of
+                                                     the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
 
         """
         Return the Qiskrypt's Quantum Circuit of the Qiskrypt's Quantum Key Exchange Protocol Round.
         """
-        return super().get_quantum_key_exchange_protocol_round_quantum_circuit()
+        return super().get_round_quantum_circuit()
+
+    def is_round_discarded(self) -> bool:
+        """
+        Return the boolean flag to keep the information about if the Qiskrypt's
+        Quantum Key Exchange Protocol Round is discarded or not.
+
+        :return super().is_round_discarded(): the boolean flag to keep the information about if the Qiskrypt's
+                                              Quantum Key Exchange Protocol Round is discarded or not.
+        """
+
+        """
+        Return the boolean flag to keep the information about if the Qiskrypt's
+        Quantum Key Exchange Protocol Round is discarded or not.
+        """
+        return super().is_round_discarded()
+
+    def set_round_discarded(self) -> None:
+        """
+        Set the boolean flag to keep the information about if the Qiskrypt's
+        Quantum Key Exchange Protocol Round is discarded or not, as True.
+        """
+
+        """
+        Set the boolean flag to keep the information about if the Qiskrypt's
+        Quantum Key Exchange Protocol Round is discarded or not, as True.
+        """
+        super().set_round_discarded()
+
+    def is_round_basis_sender_choice_made(self) -> bool:
+        """
+        Return the verification for if the basis choice made by the sender in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+
+        :return self.round_basis_sender_choice is not None: if the basis choice made by the sender in
+                                                            the Qiskrypt's DV (Discrete Variables)
+                                                            BB84 Protocol Round is already set.
+        """
+
+        """
+        Return the verification for if the basis choice made by the sender in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+        """
+        return self.round_basis_sender_choice is not None
+
+    def set_round_basis_sender_choice(self, round_basis_sender_choice: str) -> None:
+        """
+        Set the basis choice made by the sender in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is not set yet.
+
+        :param round_basis_sender_choice: the basis choice made by the sender in
+                                          the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round.
+        """
+
+        if not self.is_round_basis_sender_choice_made():
+            """
+            If the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            """
+            Set the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is not set yet.
+            """
+            self.round_basis_sender_choice = round_basis_sender_choice
+
+        else:
+            """
+            If the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            # TODO Throw - Exception
+
+    def get_round_basis_sender_choice(self) -> str:
+        """
+        Return the basis choice made by the sender in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is already set.
+
+        :return self.round_basis_sender_choice: the basis choice made by the sender in
+                                                the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round,
+                                                if it is already set.
+        """
+
+        if self.is_round_basis_sender_choice_made():
+            """
+            If the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+            """
+
+            """
+            Return the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is already set.
+            """
+            return self.round_basis_sender_choice
+
+        else:
+            """
+            If the basis choice made by the sender in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            # TODO Throw - Exception
+
+    def is_round_basis_receiver_choice_made(self) -> bool:
+        """
+        Return the verification for if the basis choice made by the receiver in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+
+        :return self.round_basis_receiver_choice is not None: if the basis choice made by the receiver in
+                                                              the Qiskrypt's DV (Discrete Variables)
+                                                              BB84 Protocol Round is already set.
+        """
+
+        """
+        Return the verification for if the basis choice made by the receiver in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+        """
+        return self.round_basis_receiver_choice is not None
+
+    def set_round_basis_receiver_choice(self, round_basis_receiver_choice: str) -> None:
+        """
+        Set the basis choice made by the receiver in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is not set yet.
+
+        :param round_basis_receiver_choice: the basis choice made by the receiver in
+                                            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round.
+        """
+
+        if not self.is_round_basis_receiver_choice_made():
+            """
+            If the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            """
+            Set the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is not set yet.
+            """
+            self.round_basis_receiver_choice = round_basis_receiver_choice
+
+        else:
+            """
+            If the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            # TODO Throw - Exception
+
+    def get_round_basis_receiver_choice(self) -> str:
+        """
+        Return the basis choice made by the receiver in
+        the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is already set.
+
+        :return self.round_basis_receiver_choice: the basis choice made by the receiver in
+                                                  the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round,
+                                                  if it is already set.
+        """
+
+        if self.is_round_basis_receiver_choice_made():
+            """
+            If the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is already set.
+            """
+
+            """
+            Return the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round, if it is already set.
+            """
+            return self.round_basis_receiver_choice
+
+        else:
+            """
+            If the basis choice made by the receiver in
+            the Qiskrypt's DV (Discrete Variables) BB84 Protocol Round is not set yet.
+            """
+
+            # TODO Throw - Exception
